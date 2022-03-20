feb3a1881fbd442a23e63681fd43b84aa9036667
Jean-Charles HARDY <jeancharles.hardy2224@gmail.com>
2022-03-20 22:56:24 +0100

    Add simulations in the csv report export

--- Packages/com.legion.dexplorer/Editor/Serializations/Serializers/Csv/CsvSerializer.cs
+++ Packages/com.legion.dexplorer/Editor/Serializations/Serializers/Csv/CsvSerializer.cs
@@ -34,29 +34,36 @@ namespace dExplorer.Editor.Serializations
 		#region Methods
 		public void Serialize(IDEAnalysisReportSerializable<T_VARIABLE> report)
 		{
-			string mainFolder = Path.Combine(_exportFolderPath, report.GetName());
+			string mainFolderPath = Path.Combine(_exportFolderPath, report.GetName());
 
-			if (Directory.Exists(mainFolder) == false)
+			if (Directory.Exists(mainFolderPath) == false)
 			{
-				Directory.CreateDirectory(mainFolder);
+				Directory.CreateDirectory(mainFolderPath);
 			}
 
 			List<float> parameterSteps = new List<float>();
+			Dictionary<float, List<float>> times = new Dictionary<float, List<float>>();
 			List<DESolvingType> solvingTypes = new List<DESolvingType>();
 			Dictionary<float, Dictionary<DESolvingType, T_VARIABLE>> meanAbsoluteErrors = new Dictionary<float, Dictionary<DESolvingType, T_VARIABLE>>();
+			Dictionary<float, Dictionary<float, List<Tuple<DESolvingType, T_VARIABLE>>>> simulationValues = new Dictionary<float, Dictionary<float, List<Tuple<DESolvingType, T_VARIABLE>>>>();
 
 			foreach (DESolvingType solvingType in report.GetSolvingTypes())
 			{
+				int index = 0;
+
 				if (solvingType != DESolvingType.ANALYTICAL)
 				{
 					solvingTypes.Add(solvingType);
+				}
 
-					foreach (Tuple<float, T_VARIABLE> meanAbsoluteErrorData in report.GetMeanAbsoluteErrors(solvingType))
+				foreach (Tuple<float, T_VARIABLE> meanAbsoluteErrorData in report.GetMeanAbsoluteErrors(solvingType))
+				{
+					float parameterStep = meanAbsoluteErrorData.Item1;
+
+					parameterSteps.Add(parameterStep);
+
+					if (solvingType != DESolvingType.ANALYTICAL)
 					{
-						float parameterStep = meanAbsoluteErrorData.Item1;
-
-						parameterSteps.Add(parameterStep);
-
 						if (meanAbsoluteErrors.ContainsKey(parameterStep) == false)
 						{
 							meanAbsoluteErrors.Add(parameterStep, new Dictionary<DESolvingType, T_VARIABLE>());
@@ -64,13 +71,47 @@ namespace dExplorer.Editor.Serializations
 
 						meanAbsoluteErrors[parameterStep].Add(solvingType, meanAbsoluteErrorData.Item2);
 					}
+
+					if (simulationValues.ContainsKey(parameterStep) == false)
+					{
+						simulationValues.Add(parameterStep, new Dictionary<float, List<Tuple<DESolvingType, T_VARIABLE>>>());
+					}
+
+					foreach (Tuple<float, T_VARIABLE> value in report.GetSimulationValues(solvingType, index))
+					{
+						if (times.ContainsKey(parameterStep) == false)
+						{
+							times.Add(parameterStep, new List<float>());
+						}
+
+						float time = value.Item1;
+						times[parameterStep].Add(time);
+						Dictionary<float, List<Tuple<DESolvingType, T_VARIABLE>>> simulations = simulationValues[parameterStep];
+
+						if (simulations.ContainsKey(time) == false)
+						{
+							simulations.Add(time, new List<Tuple<DESolvingType, T_VARIABLE>>());
+						}
+
+						simulations[time].Add(new Tuple<DESolvingType, T_VARIABLE>(solvingType, value.Item2));
+					}
+
+					index++;
 				}
 			}
 
 			parameterSteps = parameterSteps.Distinct().ToList();
 			parameterSteps.Sort();
 
-			using (StreamWriter writer = new StreamWriter(Path.Combine(mainFolder, "mean_absolute_error.csv")))
+			for (int i = 0, length = times.Count; i < length; i++)
+			{
+				float parameterStep = times.Keys.ToList()[i];
+				times[parameterStep] = times[parameterStep].Distinct().ToList();
+				times[parameterStep].Sort();
+			}
+			
+
+			using (StreamWriter writer = new StreamWriter(Path.Combine(mainFolderPath, "mean_absolute_error.csv")))
 			{
 				string header = "Parameter Step";
 
@@ -101,6 +142,49 @@ namespace dExplorer.Editor.Serializations
 					}
 
 					writer.WriteLine(line);
+				}
+			}
+
+			string simulationsFolderPath = Path.Combine(mainFolderPath, "simulations");
+			Directory.CreateDirectory(simulationsFolderPath);
+
+			foreach (float parameterStep in parameterSteps)
+			{
+				string header = "Time";
+				List<DESolvingType> currentSolvingTypes = new HashSet<DESolvingType>(solvingTypes) { DESolvingType.ANALYTICAL }.ToList();
+
+				foreach (DESolvingType solvingType in currentSolvingTypes)
+				{
+					_variableSerializer.VariableName = solvingType.ToString();
+					_variableSerializer.Variable = new T_VARIABLE();
+					_variableSerializer.Separator = _separator;
+					_variableSerializer.UpdateSerialization();
+
+					header += _variableSerializer.Separator + _variableSerializer.SerializedHeader;
+				}
+
+				using (StreamWriter writer = new StreamWriter(Path.Combine(simulationsFolderPath, string.Format("parameter_step_{0}.csv", parameterStep))))
+				{
+					writer.WriteLine(header);
+
+					foreach (float time in times[parameterStep])
+					{
+						int index = 0;
+						string line = time.ToString();
+
+						foreach (DESolvingType solvingType in currentSolvingTypes)
+						{
+							_variableSerializer.VariableName = solvingType.ToString();
+							_variableSerializer.Variable = simulationValues.ContainsKey(parameterStep) && simulationValues[parameterStep].ContainsKey(time) ? simulationValues[parameterStep][time][index].Item2 : new T_VARIABLE();
+							_variableSerializer.Separator = _separator;
+							_variableSerializer.UpdateSerialization();
+
+							index++;
+							line += _separator + _variableSerializer.SerializedVariable;
+						}
+
+						writer.WriteLine(line);
+					}
 				}
 			}
 		}
